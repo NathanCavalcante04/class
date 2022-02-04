e595fc7d308760bbe4f086778cf9f5db93f78f25
nathan cavalcante <93921282+NathanCavalcante04@users.noreply.github.com>
2022-02-04 14:04:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ola extends Component {
+  render() {
+    return (
+      <div>
+        <h1>OI,DESAFIO FEITO</h1>
+      </div>
+    );
+  }
+}
+export default ola;
