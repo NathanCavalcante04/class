703a7635dcf031c785fdea59608f68f0e1b299ef
nathan cavalcante <93921282+NathanCavalcante04@users.noreply.github.com>
2022-03-17 15:10:45 -0300

    oi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
-class ola extends Component {
+class Arrow extends Component {
+  mensagem = () => {
+    return "neymar acima de tudo e fernando acima de todos";
+  };
+
+  dobro = (x) => {
+    return x * 2;
+  };
+
   render() {
     return (
       <div>
-        <h1>OI,DESAFIO FEITO</h1>
+        <h1>{this.mensagem()}</h1>
+        <h2>{this.dobro(2)}</h2>
       </div>
     );
   }
 }
-export default ola;
+
+export default Arrow;
